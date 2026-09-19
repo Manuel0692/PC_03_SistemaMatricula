8eb0b016f44924efa91a1bf175ed777b42fbb515
Manuel <manueltizonaguilar@gmail.com>
2026-09-19 00:51:50 -0500

    feat(estudiante): define atributos, constructor y metodo toString

--- estudiante.java
+++ estudiante.java
@@ -1,1 +1,35 @@
 
+package sistemamatriculas;
+
+public class Estudiante {
+
+    private int codigo;
+    private String nombre;
+    private String carrera;
+
+    public Estudiante(int codigo, String nombre, String carrera) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.carrera = carrera;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    @Override
+    public String toString() {
+        return "Código: " + codigo
+                + " | Nombre: " + nombre
+                + " | Carrera: " + carrera;
+    }
+}
+
